66f4bc86f4f97a04837db03ec64548c60b42762b
Nikita <nikita.shalashkov@gmail.com>
2023-07-18 18:21:42 +0300

    ad

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,22 +19,6 @@ const LocaleLayout: React.FC<LocaleLayoutProps> = ({ children, params }) => {
     notFound();
   }
 
-  // function signalGooglefcPresent() {
-  //   if (!window.frames["googlefcPresent"]) {
-  //     if (document.body) {
-  //       const iframe = document.createElement("iframe");
-  //       iframe.style =
-  //         "width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;";
-  //       iframe.style.display = "none";
-  //       iframe.name = "googlefcPresent";
-  //       document.body.appendChild(iframe);
-  //     } else {
-  //       setTimeout(signalGooglefcPresent, 0);
-  //     }
-  //   }
-  // }
-  // signalGooglefcPresent();
-
   return (
     <html lang={locale}>
       <head>
@@ -43,12 +27,6 @@ const LocaleLayout: React.FC<LocaleLayoutProps> = ({ children, params }) => {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1865112126139577"
           crossOrigin="anonymous"
         ></script>
-        {/* <Script
-          async
-          src="https://fundingchoicesmessages.google.com/i/pub-1865112126139577?ers=1"
-          nonce="xG4oh1laltQ6VweQICU80g"
-        ></Script>
-        <Script id="not-block-ads " nonce="xG4oh1laltQ6VweQICU80g">(function() {signalGooglefcPresent()})();</Script> */}
       </head>
       <body className="bg-cover bg-white dark:bg-gray-900">{children}</body>
     </html>
